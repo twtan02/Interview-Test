3395c72675981193c425bb5344ff4a246f747304
twtan02 <126399779+twtan02@users.noreply.github.com>
2023-08-22 16:16:11 +0800

    Fibonacci Sequence solution

--- src/p3.py
+++ src/p3.py
@@ -10,7 +10,7 @@ def generate_fibonacci(n):
         next_element = fibonacci_sequence[-1] + fibonacci_sequence[-2]
         fibonacci_sequence.append(next_element)
         return fibonacci_sequence
-
+        
 # Ask input of the number of Fibonacci elements from user
 num_elements = int(input("Enter the number of Fibonacci sequence elements: "))
 
