e106e2d36c2a14fb3b4036f28600095d2f54956e
twtan02 <126399779+twtan02@users.noreply.github.com>
2023-08-22 16:23:31 +0800

    Node Path Existence Not Able To Solve

--- src/p9.py
+++ src/p9.py
@@ -1,1 +1,5 @@
 # Not able to solve...
+'''
+Python is a preferred choice for solving these problems due to its clear and readable syntax, 
+extensive standard library, cross-platform compatibility, strong community support, and rapid development capabilities.
+'''
