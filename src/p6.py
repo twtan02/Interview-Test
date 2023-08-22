c5b03c7391d7d771184dfe6def9f9b13cfa24b3d
twtan02 <126399779+twtan02@users.noreply.github.com>
2023-08-22 16:11:10 +0800

    Add files via upload
    Find Character with Maximum Occurence solution

---
+++ src/p6.py
@@ -0,0 +1,28 @@
+def find_max_occurrence_character(input_string):
+    # Create a dictionary to store character frequencies
+    char_count = {}
+    
+    # Remove whitespaces and punctuation, and consider case-sensitivity
+    cleaned_string = ''.join(char for char in input_string if char.isalnum())
+    
+    # Calculate character frequencies
+    for char in cleaned_string:
+        char_count[char] = char_count.get(char, 0) + 1
+    
+    # Find the character with the maximum occurrence
+    max_char = ''
+    max_count = 0
+    
+    for char, count in char_count.items():
+        if count > max_count:
+            max_char = char
+            max_count = count
+    
+    return f"Character: '{max_char}', Occurrence: {max_count}"
+
+# Input string
+input_string = "Hello, world!"
+
+# Print the result
+result = find_max_occurrence_character(input_string)
+print(result)
